0528b69ceb1fbca57f2b0cbfb5924043a878ca84
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-10-13 21:50:50 -0400

    Create function.java

---
+++ function.java
@@ -0,0 +1,13 @@
+
+    public class Main {
+  static void func() { //static void 
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    func();
+    //myMethod();
+    //myMethod();
+  }
+}
+  
