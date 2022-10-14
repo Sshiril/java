995b1fe47b94280e6e3c4e0679a5c026385aa4f0
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-10-13 21:46:39 -0400

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+// Java Program to find whether a number is even or odd
+
+    int i=5;
+    if(i%2==0){
+      System.out.println("even number");
+    }
+    else{
+      System.out.println("odd number");
+    }
+
+
+  }
+}
+  
