76828f9baa1e66ad741ae447a547f5d52ba19618
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-10-13 21:49:17 -0400

    Create whileevenodd.java

---
+++ whileevenodd.java
@@ -0,0 +1,16 @@
+//Program to display first n odd and even numbers
+class Main {
+  public static void main(String[] args) {
+
+  int i=1; //intialization
+    int n=40;
+
+    System.out.println("even numbers");
+    while(i<n){
+      System.out.println(i);
+   
+      i=i+2;
+    }
+ 
+  }
+}
